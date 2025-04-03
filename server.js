58b349ab3cfc7b0cf552d98657da919b3f1d0311
heeltechsolutions <lokeshheeltech@gmail.com>
2025-04-03 18:14:13 +0530

    Update server.js
    new onre

--- server.js
+++ server.js
@@ -9,10 +9,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(cors({
-  origin: "http://localhost:5173", // Allow requests from this frontend
-  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
-  allowedHeaders: ["Content-Type", "Authorization"] // Allowed headers
+  origin: ["http://localhost:5173", "https://app.mmrtechsolutions.co.in"], // Allow both local and production frontend
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
